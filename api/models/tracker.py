8debd6774540e59743f0a0135426872519cf0f9b
rubysattar <rubysattar@gmail.com>
2020-10-06 11:45:54 -0400

    tracker model and views

---
+++ api/models/tracker.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+class Tracker(models.Model):
+  # define fields
+  # https://docs.djangoproject.com/en/3.0/ref/models/fields/
+  TherapyType = (
+    ('Chemotherapy'),
+    ('Radiation'),
+    ('Hormonal'),
+    ('Biological'),
+    ('Surgery')
+  )
+
+  Symptoms = (
+    ('nausea'),
+    ('hair loss'),
+    ('anemia'),
+    ('fatigue')
+  )
+
+  owner = models.ForeignKey(
+      get_user_model(),
+      therapyType = models.Charfield(
+        max_length = 20,
+        choices = TherapyType,
+        default = 'Chemotherapy'
+      ),
+      symptoms=models.NullBooleanField(
+        max_length = 20,
+        choices = Symptoms
+      ),
+      related_name = 'tracker',
+      on_delete = models.CASCADE
+  )
+
+  def __str__(self):
+    # This must return a string
+    return f"The therapy type of this therapy is '{self.therapy_type}'."
+
+  def as_dict(self):
+    """Returns dictionary version of Deck models"""
+    return {
+        'id': self.id,
+        'therapy_type': self.therapy_type,
+        'symptoms':
+    }
